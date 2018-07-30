474d27a9caa92d323b5b4298f4787482724bd561
Rafael Bacus <rafael.bacus@rhino.local>
2018-07-30 11:02:43 -0400

    Added css and js minify to gulp tasks

--- Web/gulpfile.js
+++ Web/gulpfile.js
@@ -2,21 +2,41 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var minify = require('gulp-minify');
+var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var clean = require('gulp-rimraf');
+var util = require('gulp-util');
+var cssmin = require('gulp-cssmin');
 
-var input = 'Styles/*.scss';
-var output = 'wwwroot/css/';
+var cssInput = 'Styles/*.scss';
+var cssOutput = 'wwwroot/css/';
 var sassOptions = {style: 'expanded'};
 var autoprefixerOptions = {browsers: ['last 10 versions']};
 var renameOptions = {suffix: '.min'};
 
 gulp.task('css', function(){
-    gulp.src(input)
+    gulp.src(cssOutput + "*", { read : false})
+        .pipe(clean());
+
+    gulp.src(cssInput)
         .pipe(sass(sassOptions))
         .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest(output))
-        .pipe(minify())
+        .pipe(gulp.dest(cssOutput))
+        .pipe(cssmin({level: 2}))
         .pipe(rename(renameOptions))
-        .pipe(gulp.dest(output))
+        .pipe(gulp.dest(cssOutput));
+});
+
+var jsInput = 'Scripts/*.js';
+var jsOutput = 'wwwroot/js/';
+
+gulp.task('js', function(){
+    gulp.src(jsOutput + "*", { read : false})
+        .pipe(clean());
+
+    gulp.src(jsInput)
+        .pipe(gulp.dest(jsOutput))
+        .pipe(uglify())
+        .pipe(rename(renameOptions))
+        .pipe(gulp.dest(jsOutput));
 });
