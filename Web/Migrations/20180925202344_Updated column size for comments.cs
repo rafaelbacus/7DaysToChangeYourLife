8db2313c141b15f1ad709be8a7d9814d16c8edc3
rafael.bacus <rafael.bacus@gmail.com>
2018-09-25 16:27:08 -0400

    Added ef migration

---
+++ Web/Migrations/20180925202344_Updated column size for comments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Migrations
+{
+    public partial class Updatedcolumnsizeforcomments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                maxLength: 4096,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 1024);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 4096);
+        }
+    }
+}
